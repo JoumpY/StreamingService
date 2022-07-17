ff7fc25689683746aebe0758a3bbda57c301fc84
Jumpi <63369072+JoumpY@users.noreply.github.com>
2022-07-17 22:21:27 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: "Matrice"),
+      theme: ThemeData(fontFamily: "Lato"),
       debugShowCheckedModeBanner: false,
       title: "Splital",
       home: const PageLayout(),
